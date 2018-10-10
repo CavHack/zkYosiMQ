c82c1153a1435b082a6130a3599cdeeeaefe089d
CavHack <karl@blackesquirel.com>
2018-10-10 15:00:35 -0400

    SKObserver
    A recurring wtiness

---
+++ src/verifier/SolomonKey/SKObserver.hpp
@@ -0,0 +1,35 @@
+/*****
+ *
+ *
+ * @file SKObserver.hpp
+ * @brief Header for Observer 
+ *
+ *
+ *
+ *
+ */
+
+#pragma once
+#ifndef __SK_OBSERVER_HPP
+#define __SK_OBSERVER_HPP
+
+#include<lagrangian/Linear.hpp>
+
+#include<memory>
+
+namespace zkYosiMQ {
+
+  class SKObserver {
+
+  public:
+    typedef 
+    typedef std::unique_ptr<const L>
+
+
+}
+
+
+
+
+
+}
